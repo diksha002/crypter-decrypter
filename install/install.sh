ec64993d6373105d1427173cc26e60e2cb698aba
diksha002 <61411982+diksha002@users.noreply.github.com>
2020-09-20 18:47:34 +0400

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -12,3 +12,11 @@ mkdir decrypted
 
 cp /home/user/crypter-decrypter/service/crypt.service /etc/systemd/system/
 cp /home/user/crypter-decrypter/service/decrypt.service /etc/systemd/system/
+
+cd ~/etc/systemd/system
+
+sudo systemctl start crypt
+sudo systemctl enable crypt
+
+sudo systemctl start decrypt
+sudo systemctl enable decrypt
